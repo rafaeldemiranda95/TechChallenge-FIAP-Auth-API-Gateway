c849b3a6d896932656f5b2bf1eb0dd3a84275e98
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2024-03-16 14:41:18 -0300

    Update firebase.php

--- config/firebase.php
+++ config/firebase.php
@@ -54,7 +54,7 @@ return [
                 'type' => env('FIREBASE_TYPE'),
                 'project_id' => env('FIREBASE_PROJECT_ID'),
                 'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'),
-                'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+                'private_key' =>  env('FIREBASE_PRIVATE_KEY'),
                 'client_email' => env('FIREBASE_CLIENT_EMAIL'),
                 'client_id' => env('FIREBASE_CLIENT_ID'),
                 'auth_uri' => env('FIREBASE_AUTH_URI'),
